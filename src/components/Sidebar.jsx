b611cf06fd6323beaa9a4a4fcf2fbb21d7bc8d94
Hruaia <hruaitluangasailo20@gmail.com>
2026-02-23 18:11:45 +0530

    lolsssssss

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../styles/sidebar.css";
 import logo from "../assets/Vector.svg";
 
@@ -11,11 +11,6 @@ import { RiLiveLine } from "react-icons/ri";
 import { AiOutlineFileDone } from "react-icons/ai";
 
 export default function Sidebar() {
-  const location = useLocation();
-
-  // ONLY open submenu when current route is inside subjects
-  const isSubjectsActive = location.pathname.startsWith("/subjects");
-
   return (
     <aside className="sidebar">
       {/* Brand */}
@@ -23,12 +18,15 @@ export default function Sidebar() {
         <img src={logo} alt="Logo" className="sidebar__logoCircle" />
         <div>
           <h2 className="sidebar__title">ShikshaCom</h2>
-          <p className="sidebar__tagline">Empowerment Through Education</p>
+          <p className="sidebar__tagline">
+            Empowerment Through Education
+          </p>
         </div>
       </div>
 
       {/* Links */}
       <nav className="sidebar__nav">
+        {/* Dashboard */}
         <NavLink className="sidebar__link" to="/" end>
           <span className="sidebar__icon">
             <MdDashboardCustomize />
@@ -36,34 +34,47 @@ export default function Sidebar() {
           Dashboard
         </NavLink>
 
+        {/* Subjects */}
         <NavLink className="sidebar__link" to="/subjects">
           <span className="sidebar__icon">
             <BsBook />
           </span>
-          Subject
+          Subjects
         </NavLink>
 
-        {/* submenu appears only if subject is active */}
-        {isSubjectsActive && (
-          <div className="sidebar__subMenu">
-            <NavLink className="sidebar__subLink" to="/subjects/assignments">
-              <FaClipboardList /> <span>Assignment</span>
-            </NavLink>
+        {/* Assignments (COURSE LEVEL) */}
+        <NavLink className="sidebar__link" to="/assignments">
+          <span className="sidebar__icon">
+            <FaClipboardList />
+          </span>
+          Assignments
+        </NavLink>
 
-            <NavLink className="sidebar__subLink" to="/subjects/quiz">
-              <AiOutlineFileDone /> <span>Quiz</span>
-            </NavLink>
+        {/* Quiz */}
+        <NavLink className="sidebar__link" to="/quiz">
+          <span className="sidebar__icon">
+            <AiOutlineFileDone />
+          </span>
+          Quiz
+        </NavLink>
 
-            <NavLink className="sidebar__subLink" to="/subjects/recordings">
-              <BiVideo /> <span>Recordings</span>
-            </NavLink>
+        {/* Recordings */}
+        <NavLink className="sidebar__link" to="/recordings">
+          <span className="sidebar__icon">
+            <BiVideo />
+          </span>
+          Recordings
+        </NavLink>
 
-            <NavLink className="sidebar__subLink" to="/subjects/study-material">
-              <FaBookOpen /> <span>Study Material</span>
-            </NavLink>
-          </div>
-        )}
+        {/* Study Material */}
+        <NavLink className="sidebar__link" to="/study-material">
+          <span className="sidebar__icon">
+            <FaBookOpen />
+          </span>
+          Study Material
+        </NavLink>
 
+        {/* Live Sessions */}
         <NavLink className="sidebar__link" to="/live-sessions">
           <span className="sidebar__icon">
             <RiLiveLine />
@@ -74,6 +85,3 @@ export default function Sidebar() {
     </aside>
   );
 }
-
-
-
